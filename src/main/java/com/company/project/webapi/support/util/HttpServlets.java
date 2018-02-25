4e3358b9a1deb540d93cd71494abbc9ff3b31f43
wangzhenjun <wangzhenjun@okay.cn>
2018-02-25 15:11:13 +0800

    更新代码

---
+++ src/main/java/com/company/project/webapi/support/util/HttpServlets.java
@@ -0,0 +1,17 @@
+package com.company.project.webapi.support.util;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HttpServlets {
+
+    public static HttpServletRequest toHttp(ServletRequest request) {
+        return (HttpServletRequest) request;
+    }
+
+    public static HttpServletResponse toHttp(ServletResponse response) {
+        return (HttpServletResponse) response;
+    }
+}
---
+++ src/main/java/com/company/project/webapi/support/util/JsonUtil.java
@@ -0,0 +1,9 @@
+package com.company.project.webapi.support.util;
+
+public class JsonUtil {
+
+
+    public static <T> String toJson(T t) {
+        return null;
+    }
+}
