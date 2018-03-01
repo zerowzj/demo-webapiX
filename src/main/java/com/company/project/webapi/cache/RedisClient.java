56ff44e94d7c0a0cfc8627ee1618e6665ff58288
wangzhenjun <wangzhenjun@okay.cn>
2018-03-01 18:14:28 +0800

    更新代码

--- src/main/java/com/company/project/webapi/cache/RedisClient.java
+++ src/main/java/com/company/project/webapi/cache/RedisClient.java
@@ -4,23 +4,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import redis.clients.jedis.Jedis;
 import redis.clients.util.Pool;
 
+import javax.annotation.PostConstruct;
+
 /**
+ * 静态变量/类变量不是对象的属性,而是一个类的属性,spring则是基于对象层面上的依赖注入。
+ *
  * @author wangzhj
  */
 public class RedisClient {
 
+    @Autowired
+    private Pool<Jedis> pool;
+
     private static Pool<Jedis> POOL;
 
+    //通过中间变量赋值
+    @PostConstruct
+    public void init() {
+        POOL = pool;
+    }
+
     public static void set(String key, String value) {
-        getJedis().set(key, value);
+        Jedis jedis = getJedis();
+        try {
+            jedis.set(key, value);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+//            jedis.close();
+        }
     }
 
     private static Jedis getJedis() {
         return POOL.getResource();
     }
 
-    @Autowired
-    public void setPool(Pool<Jedis> pool) {
-        POOL = pool;
-    }
+    //spring支持set方法注入，利用非静态setter 方法注入静态变量
+//    @Autowired
+//    public void setPool(Pool<Jedis> pool) {
+//        POOL = pool;
+//    }
 }
