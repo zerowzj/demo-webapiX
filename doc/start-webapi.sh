d626407de69c3739facebf2beefaffd7449fdc94
wangzhenjun <wangzhenjun@okay.cn>
2018-07-06 15:51:20 +0800

    更新代码

--- doc/start-webapi.sh
+++ doc/start-webapi.sh
@@ -8,8 +8,10 @@ cd `dirname $0`
 pid=`ps -ef |grep $PROJECT_NAME |grep -v grep |awk '{print $2}'`
 
 if [ -n "$pid" ]; then
- kill -9 $pid
- echo "shutdown tomcat..."
+    kill -9 $pid
+    echo "------------------------------"
+    echo "     Shutdown tomcat...       "
+    echo "------------------------------"
 fi
 
 cd $DEPLOY_HOME/bin
